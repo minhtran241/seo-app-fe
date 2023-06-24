43d591c6435b8134d6f2a057514d09183fc51037
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-06-24 06:17:00 -0400

    update map

--- components/Contact/Map.tsx
+++ components/Contact/Map.tsx
@@ -3,7 +3,7 @@ const Map = () => {
     <div className="block w-full shrink-0 grow-0 basis-auto">
       <div className="w-full" style={{ height: '500px' }}>
         <iframe
-          src="https://maps.google.com/maps?q=Tin Học Pama&t=&z=10&ie=UTF8&iwloc=&output=embed"
+          src="https://www.google.com/maps/embed/v1/place?q=TRUYỀN+THÔNG+VÀ+TIN+HỌC+PAMA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           className="left-0 top-0 h-full w-full"
           allowFullScreen
         ></iframe>
